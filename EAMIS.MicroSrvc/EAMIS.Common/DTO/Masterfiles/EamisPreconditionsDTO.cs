3d736878be22e5199c6b61b8f78bcc2ed6740709
npalangan <90250891+npalangan@users.noreply.github.com>
2022-05-12 20:21:36 +0800

    Delete EAMIS.MicroSrvc directory

--- EAMIS.MicroSrvc/EAMIS.Common/DTO/Masterfiles/EamisPreconditionsDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EAMIS.Common.DTO.Masterfiles
-{
-    public class EamisPreconditionsDTO
-    {
-        public int Id { get; set; }
-        public int Parent_Id { get; set; }
-        public string Precondition_Description { get; set; }
-    }
-}
